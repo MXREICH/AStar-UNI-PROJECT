cf03329933214b6decfd24ecc36364a13b0a63c8
REICHERT Maximilien <reichertmaximilien@gmail.com>
2023-02-17 16:51:43 +0100

    Remove unecessary `heap` class

--- code/classes/heap.py
+++ code/classes/heap.py
@@ -1,10 +1,13 @@
 class Node:
-    def __init__(self, cube_array, arm, heuristic_value, cost_value):
+    def __init__(self, cube_array, arm, heuristic_value, cost_value, parent, children):
         self.cube_array = cube_array
         self.arm = arm
         self.heuristic_value = heuristic_value
         self.cost_value = cost_value
+        self.parent = parent
+        self.children = children
+
     def __hash__(self):
         return hash((''.join((map(str, self.cube_array))), self.arm, self.heuristic_value, self.cost_value))
 
-#class Heap:
+    
