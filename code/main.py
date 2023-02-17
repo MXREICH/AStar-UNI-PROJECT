a76e9e5201dbb124d1a4743d4b9665b0515e2105
REICHERT Maximilien <reichertmaximilien@gmail.com>
2023-02-17 16:36:41 +0100

    Change `main` to test newly added hash functions

--- code/main.py
+++ code/main.py
@@ -1,4 +1,14 @@
 import anytree
+
+from classes.cube import Cube
+from classes.heap import Node
+from classes.arm import Arm
+
 if __name__ == '__main__':
-    print(f'hii')
-#zozo
+    A = Cube(0, True, False, True)
+    B = Cube(0, True, False, True)
+    Arm = Arm()
+    NodeA = Node([A,B], Arm, 0, 0)
+    NodeB = Node([A,B], Arm, 0, 0)
+    NodeC = Node([A,B], Arm, 1, 0)
+    print("A:",hash(NodeA), "B:", hash(NodeB), "C:", hash(NodeC))
