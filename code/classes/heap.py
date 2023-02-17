729c4e22b658034372efb6ea7fc1643209a32f75
REICHERT Maximilien <reichertmaximilien@gmail.com>
2023-02-17 15:25:10 +0100

    heap first implementation

--- code/classes/heap.py
+++ code/classes/heap.py
@@ -1,4 +1,9 @@
 class Node:
+    def __init__(self, cube_array, arm, heuristic_value, cost_value):
+        self.cube_array = cube_array
+        self.arm = arm
+        self.heuristic_value = heuristic_value
+        self.cost_value = cost_value
 
 class Heap:
 
